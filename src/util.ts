98267a80ba91b20a5f796dbf7d1ff2d5bfdcf80b
musemy <musemy4@gmail.com>
2022-02-16 13:55:44 +0900

    기본 완성

--- src/util.ts
+++ src/util.ts
@@ -1,31 +1,29 @@
 // const resultIs = (input1: number, input2: number, operator: '+' | '-' | '/' | '*') => {
 const resultIs = () => {
-    const input1 = document.getElementById('prev');
-    const input2 = document.getElementById('next');
+    const input1 = (<HTMLInputElement>document.getElementById('prev'));
+    const input2 = (<HTMLInputElement>document.getElementById('next'));
     const operator = document.getElementById('operator');
     const result = document.getElementById('result');
 
-    if(input1?.innerHTML === '' || input1?.innerHTML === undefined) {
+    if(input1?.value === '' || input1 === undefined) {
         input1?.focus();
     }
-    if(input2?.innerHTML === '' || input2?.innerHTML === undefined) {
+    if(input2?.value === '' || input2 === undefined) {
         input2?.focus();
     }
 
     let resultNumber = 0;
-    console.log(input1?.innerHTML, input2?.innerHTML, operator?.innerHTML);
+
     if (operator?.innerHTML === '+') {
-        resultNumber = Number(input1?.innerHTML) + Number(input2?.innerHTML);
+        resultNumber = Number(input1?.value) + Number(input2?.value);
     } else if (operator?.innerHTML === '-') {
-        resultNumber = Number(input1?.innerHTML) - Number(input2?.innerHTML);
+        resultNumber = Number(input1?.value) - Number(input2?.value);
     } else if (operator?.innerHTML === '/') {
-        resultNumber = Number(input1?.innerHTML) / Number(input2?.innerHTML);
+        resultNumber = Number(input1?.value) / Number(input2?.value);
     } else {
-        resultNumber = Number(input1?.innerHTML) * Number(input2?.innerHTML);
+        resultNumber = Number(input1?.value) * Number(input2?.value);
     }
 
-    console.log(resultNumber);
-    
     if(result) {
         result.innerText = resultNumber.toString();
     }
